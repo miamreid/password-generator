7504662e73a34e4593363fb00a19c51ada510adc
Mia Reid <miareid@MR-ST-G2H9P1FJPV.local>
2023-03-13 17:19:16 -0400

    cleaned js code and made comments

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Assignment Code
+
+//Creates variable to pull out the #generate id on the generate button within the html
 var generateBtn = document.querySelector("#generate");
 
+//Creates a variable that acts as a placeholder in thet password text area until a password is generated
 var passwordText = document.querySelector("#password");
 
+//this Object holds the properties and functions that randomize characters, numbers, etc.
 var passwordFunc = {
   symbols: specialCharacters,
   numbers: numericCharacters,
@@ -10,6 +14,7 @@ var passwordFunc = {
   upper: uppercaseCharacters
 }
 
+//Event listener for when user clicks the "generate" button,a series of prompts begins
 generateBtn.addEventListener("click", function() {
   var passwordLength = Number(window.prompt("How many characters would you like your password to contain?"));
   //Checks that password length is greater than 8 characters but less than 128
@@ -29,25 +34,31 @@ generateBtn.addEventListener("click", function() {
 
   var upperPrompt = confirm("Click OK to confirm including uppercase characters.");
   console.log("Uppercase characters: " + upperPrompt);
-
+//end of window prompts
   passwordText.innerText = generatePassword(symbolsPrompt, numbersPrompt, lowerPrompt, upperPrompt, passwordLength);
 });
 
+//Function that pulls the randomize functions together to generate password
 function generatePassword(symbols, numbers, lower, upper, passwordLength) {
 
+//left as an empty string to initialize before function ends
   var generatedPassword = '';
 
+//counts how many types user confirms to use in strong password
   var funcTypes = lower + upper + numbers + symbols;
   console.log('funcTypes: ', funcTypes);
 
+  //filters out the function types that are not selected or shown as "true" in an array
   var arrTypes = [{lower}, {upper}, {numbers}, {symbols}].filter
   (item => Object.values(item)[0]);
   console.log('arrTypes: ', arrTypes);
 
+//if none of the arrays are true, the function stops
   if (arrTypes === false) {
     return;
   }
 
+//a for loop that loops each function type that is true, and runs their randomized functions identified in the passwordFunc object which is appended
   for(let i = 0; i <= passwordLength; i += funcTypes) {
     arrTypes.forEach(type => {
       var funcName = Object.keys(type)[0];
@@ -55,13 +66,13 @@ function generatePassword(symbols, numbers, lower, upper, passwordLength) {
       generatedPassword += passwordFunc [funcName]();
       });
   }
-
+//assigns the final password to equal generated password
   var finalPassword = generatedPassword;
 
   return finalPassword;
 }
 
-
+//functions to randomize symbols, numeric characters, lowercase characters, and uppercase characters from 
 function specialCharacters() {
   const symbols = '!@#$%^&*(){}[]=<>/,.';
   return symbols[Math.floor(Math.random() * symbols.length)];
